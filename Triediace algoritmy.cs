3d550b6603c9cf070b18b7a572cf2a8b5c4ed311
ricHARD951 <75687540+ricHARD951@users.noreply.github.com>
2023-02-02 19:23:55 +0100

    Update Triediace algoritmy.cs

--- Triediace algoritmy.cs
+++ Triediace algoritmy.cs
@@ -89,17 +89,14 @@ static void SelectionSort(int[] arr, int n)
 {
     for (int i = 0; i < n - 1; i++)
     {
-        int minIndex = i;
+        int min_idx = i;
         for (int j = i + 1; j < n; j++)
-        {
-            if (arr[j] < arr[minIndex])
-            {
-                minIndex = j;
-            }
-        }
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
 
-        int temp = arr[minIndex];
-        arr[minIndex] = arr[i];
+        
+        int temp = arr[min_idx];
+        arr[min_idx] = arr[i];
         arr[i] = temp;
     }
 }
