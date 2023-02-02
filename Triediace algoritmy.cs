a58f2ae58401c3beb14f98d1d73c3afb4a686e8d
ricHARD951 <75687540+ricHARD951@users.noreply.github.com>
2023-02-02 19:19:45 +0100

    Update Triediace algoritmy.cs

--- Triediace algoritmy.cs
+++ Triediace algoritmy.cs
@@ -1,4 +1,4 @@
-//Insertion_sort
+//InsertionSort
 int length = 50000;
 int[] arr = new int[length];
 Random random = new Random();
@@ -67,7 +67,42 @@ static void Bubblesort(int[] arr, int n)
 }
 
 
+//SelectionSort
+int length = 50000;
+int[] arr = new int[length];
+Random random = new Random();
 
+for (int i = 0; i < length; i++)
+{
+    arr[i] = random.Next();
+}
+
+
+var watch = new System.Diagnostics.Stopwatch();
+watch.Start();
+SelectionSort(arr, arr.Length);
+watch.Stop();
+Console.WriteLine($"Dĺžka triedenia : {watch.Elapsed.TotalSeconds} sekund");
+
+
+static void SelectionSort(int[] arr, int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+
+        int temp = arr[minIndex];
+        arr[minIndex] = arr[i];
+        arr[i] = temp;
+    }
+}
 
 
 
